2969ea988af74978346abb24638fc3b52ea12b89
dyep49 <dyep49@gmail.com>
2014-11-22 19:42:18 -0500

    moves src into scripts

--- src/routes/beta/beta.controller.js
+++
@@ -1,4 +0,0 @@
-module.exports = /* @ngInject */ function() {
-  'use strict';
-
-};
---
+++ src/scripts/components/panel/panel.directive.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+  'use strict';
+
+  return {
+    restrict: 'E',
+    templateUrl: 'src/components/panel/panel.template.html',
+    scope: {},
+    link: function(scope, element) {
+      
+    }
+  }
+}
